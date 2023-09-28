693ef2300c445e9106769baf19c90ed915022144
Pavel <nightcharm@mail.ru>
2023-09-29 00:48:09 +0300

    add footer Navbar

--- client/src/app/components/common/footer/footerNavBar/footerNavBar.tsx
+++ client/src/app/components/common/footer/footerNavBar/footerNavBar.tsx
@@ -1,17 +1,18 @@
 import styles from "./footerNavBar.module.scss";
 import NavBar from "../../navBar/navBar";
+import { Link } from "react-router-dom";
 const FooterNavBar = () => {
   return (
     <div className={styles.footer__navigate}>
-      <div>
+      <div className={styles.footer__navigate__logo}>
         <img src="/img/icons/icons-social-footer.svg" alt="social" />
       </div>
       <div className={styles.footer__navigate__bar}>
         <NavBar />
       </div>
 
-      <p>2020 © Kinoarea. Все права защищены</p>
-      <p>Политика конфиденциальности</p>
+      <p className={styles.footer__navigate__license}>2023 © Kinoarea. Все права защищены</p>
+      <Link to={"404"} className={styles.footer__navigate__confedence}>Политика конфиденциальности</Link>
     </div>
   );
 };
