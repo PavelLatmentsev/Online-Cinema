8b6c5a792eb1f755fe3dbfa4c6733690249e52c9
Pavel <nightcharm@mail.ru>
2023-10-12 23:10:33 +0300

    add now in cinema component

--- client/src/app/components/pages/mainPage/mainPage.tsx
+++ client/src/app/components/pages/mainPage/mainPage.tsx
@@ -1,6 +1,7 @@
 import genreService from "../../../service/genre.service";
 import { useQuery } from "@tanstack/react-query";
 import { useGenreStore } from "../../../store/genreStore";
+import styles from "./mainPage.module.scss";
 const MainPage = () => {
   const genre=useGenreStore(state=>state.genre);
   const setDatas=useGenreStore(state=>state.setData);
@@ -20,7 +21,29 @@ const { isLoading, isSuccess, error, data } = useQuery(
 
   return (
     <main>
-
+      <section>
+        <div className={styles.container}>
+           <div className={styles.nowCinema}>
+           <div>
+            <h1>Сейчас в кино</h1>
+            <div></div>
+            <div>
+              <ul>
+                <li>Все</li>
+                <li>Боевики</li>
+                <li>Приключения</li>
+                <li>Комедии</li>
+                <li>Фантастика</li>
+                <li>Триллеры</li>
+                <li>Драма</li>
+              </ul>
+            </div>
+           </div>
+           <div></div>
+           <button>Все новинки</button>
+           </div>
+        </div>
+      </section>
     </main>
   );
 };
