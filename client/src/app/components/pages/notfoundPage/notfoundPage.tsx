62f32afcd4b675f33773e7a55b96695b2278a5ef
Pavel <nightcharm@mail.ru>
2023-10-17 20:31:37 +0300

    add notFound Page

--- client/src/app/components/pages/notfoundPage/notfoundPage.tsx
+++ client/src/app/components/pages/notfoundPage/notfoundPage.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import CommonButton from "../../common/button/commonButton";
+import styles from "./notfoundPage.module.scss"
 const NotfoundPage = () => {
-  return <h1>404</h1>;
+  return (
+    <div className={styles.notFound}>
+      <h1 className={styles.notFound__error}>404</h1>
+      <h2 className={styles.notFound__title}>Кина не будет:( </h2>
+      <p className={styles.notFound__description}>
+        Возможно, данного адреса страницы не сущетсвует, или странциа была
+        перемещена.
+      </p >
+      <div className={styles.notFound__btn}>
+        <Link to="/"> <CommonButton background="#3657CB">Вернуться на главную</CommonButton> </Link> <CommonButton> Поиск по сайту</CommonButton>
+      </div>
+      
+    </div>
+  );
 };
 
 export default NotfoundPage;
