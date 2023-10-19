79e96add4fd517446c604bdcf515d50d04762a3d
Pavel <nightcharm@mail.ru>
2023-10-19 20:06:15 +0300

    change schema Movie

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -7,7 +7,27 @@ const schema = new Schema(
     rating: { type: String },
     url: { type: String },
     isNewCinema: { type: Boolean },
-    inCinema: { type: Boolean }
+    inCinema: { type: Boolean },
+    originalName: { type: String },
+    ratingKinoarea: { type: String },
+    description: { type: String },
+    trailerUrl: { type: String }, 
+    expectations: { type: String },
+    favorites: { type: Boolean },
+    release: { type: String },
+    country: { type: String },
+    tagline: { type: String },
+    director: { type: String },
+    scenarist: { type: String },
+    producer: { type: String },
+    operator: { type: String },
+    composer: { type: String },
+    artist: { type: String },
+    installation: { type: String },
+    premiereWorld: { type: String },
+    premiereRF: { type: String },
+    age: { type: String },
+    timeMovie: { type: String },
   },{ versionKey: false }
 );
 
