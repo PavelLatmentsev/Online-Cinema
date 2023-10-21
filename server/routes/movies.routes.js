7326936eb72bd7f7617170a7f8119e6fc959be39
Pavel <nightcharm@mail.ru>
2023-10-21 03:58:42 +0300

    fix mock data in movies

--- server/routes/movies.routes.js
+++ server/routes/movies.routes.js
@@ -15,14 +15,13 @@ router.get("/", async(req,res) =>{
 
 router.get("/:id", async(req,res) =>{
     try {
-        const {id} = req.params
-        const movie = await Movie.findById(id)
+        const {id} = req.params;
+        const movie = await Movie.findById(id);
         if(movie) {
              return res.status(200).send(movie)
         } else {
             return  res.status(401).json({message: 'movie not found'})
         }
-      
     } catch (e) {
         res.status(500).json({
             message:"При поиске фильма произошла ошибка"
